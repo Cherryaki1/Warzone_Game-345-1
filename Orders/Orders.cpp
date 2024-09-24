974504fd47a43ccf80407ebf0b11b4ad4839dbd9
Cherryaki1 <157229478+Cherryaki1@users.noreply.github.com>
2024-09-24 15:26:47 -0400

    Orders methods instantiated

---
+++ Orders/Orders.cpp
@@ -0,0 +1,52 @@
+#include <list>
+#include <string>
+#include <queue>
+#include <stdbool.h>
+
+class Order {
+    public:
+
+        bool validate() {
+
+        }
+
+        void execute() {
+
+        }
+
+        void deploy() {
+
+        }
+
+        void advance() {
+
+        }
+
+        void bomb() {
+
+        }
+
+        void  blockage() {
+
+        }
+
+        void airlift() {
+
+        }
+
+        void negotiate() {
+            
+        }
+};
+
+class OrderList {
+    public:
+
+        void move() {
+
+        }
+
+        void remove() {
+
+        }
+};
