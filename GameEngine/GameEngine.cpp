21f833d84e02eb601bf615540e3b5bd6a76ab2c8
artsa <artsanes68@gmail.com>
2024-09-30 13:13:51 -0400

    Transition States is done, just have to add the actual code in them.

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -2,7 +2,31 @@
 #include "GameEngine.h"
 using namespace std;
 
-bool GameEngine::startUpPhase() {
+// AT THE END OF EACH METHOD, CHECK IF THE COMMAND ENTERED TO TRANSITION IS VALID, ELSE ERROR MSG
+bool StartUp::startUpPhase() {
     cout << "Starting up Phase ..." << endl;
     return true; // True if phase is done
 }
+
+bool Play::reinforcementPhase() {
+    cout << "Reinforcement Phase ..." << endl;
+    return true;
+}
+
+bool Play::ordersExecutionPhase() {
+    cout << "Orders Execution Phase ..." << endl;
+    return true;
+}
+
+bool Play::ordersIssuingPhase() {
+    cout << "Orders Issuing Phase ..." << endl;
+    return true;
+}
+
+bool Play::endPhase() {
+    cout << "End Phase ..." << endl;
+    return true;
+}
+
+
+
