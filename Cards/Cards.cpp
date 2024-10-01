9d672c4ff84ea5d259e3b7f0ba9088cdbdceedfe
Vala Khalilzadeh <71854922+vala35@users.noreply.github.com>
2024-10-01 15:20:15 -0400

    Cards (#2)
    * test
    
    * basic header completion
    
    * basic header completion
    
    * cards: function and constructor signatures
    
    * finished card signatures for A1
    
    * finished card signatures for A1
    
    * Added card sig skeleton in Cards.cpp
    
    * display() function for hand obj
    
    * removed using std namespace in card.h
    
    * return to deck function

---
+++ Cards/Cards.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "Cards.h"
+using namespace std;
+
+string cardTypeLookup[5] = {"Bomb", "Reinforcement", "Blockade", "Airlift", "Diplomacy"};
+
+//CARDS
+Card::Card() {
+
+}
+Card::~Card() {}
+Card::Card(string type) {}
+Card::Card(int type) {}
+
+//void Card::setType(int type) {}
+//void Card::setType(string type) {}
+
+int Card::getTypeID() {}
+string Card::getType() {}
+
+bool Card::play() {}
+
+
+//DECK
+Deck::Deck() {}
+Deck::~Deck() {}
+
+void Deck::initialize() {}
+void Deck::initialize(int numCards) {}
+
+Card *Deck::draw() {}
+void Deck::shuffle() {}
+
+
+
+//HAND
+Hand::Hand() {}
+Hand::~Hand() {}
+
+void Hand::display() {}
+
+void Hand::addToHand(Card *card) {}
