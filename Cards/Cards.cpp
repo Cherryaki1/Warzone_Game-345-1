dffa579f2e4fb6bca8c6376edd07b80416a44373
vala35 <valakhalilzadeh@gmail.com>
2024-10-01 13:52:31 -0400

    Added card sig skeleton in Cards.cpp

--- Cards/Cards.cpp
+++ Cards/Cards.cpp
@@ -2,5 +2,38 @@
 #include "Cards.h"
 using namespace std;
 
-//string cardTypeLookup[5] = {"Bomb", "Reinforcement", "Blockade", "Airlift", "Diplomacy"};
+string cardTypeLookup[5] = {"Bomb", "Reinforcement", "Blockade", "Airlift", "Diplomacy"};
 
+//CARDS
+Card::Card() {}
+Card::~Card() {}
+Card::Card(string type) {}
+Card::Card(int type) {}
+
+void Card::setType(int type) {}
+void Card::setType(string type) {}
+
+int Card::getTypeID() {}
+string Card::getType() {}
+
+bool Card::play() {}
+
+
+
+//DECK
+Deck::Deck() {}
+Deck::~Deck() {}
+
+void Deck::initialize() {}
+void Deck::initialize(int numCards) {}
+
+Card *Deck::draw() {}
+void Deck::shuffle() {}
+
+
+
+//HAND
+Hand::Hand() {}
+Hand::~Hand() {}
+
+void Hand::addToHand(Card *card) {}
